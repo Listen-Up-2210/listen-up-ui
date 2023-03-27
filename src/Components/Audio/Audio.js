c67a5ae952c5cb116d33d0541f6e2f064d7bf8e2
Angela Wirth <angiewirth92@gmail.com>
2023-03-27 13:18:45 -0400

    Add new components and their respective imports and exports

---
+++ src/Components/Audio/Audio.js
@@ -0,0 +1,7 @@
+import React from "react";
+import './Audio.css'
+
+
+
+
+export default Audio
---
+++ src/Components/Difficulty/Difficulty.js
@@ -0,0 +1,7 @@
+import React from "react";
+import './Difficulty.css'
+
+
+
+
+export default Difficulty
