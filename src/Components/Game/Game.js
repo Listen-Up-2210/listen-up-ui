ff9faeb6326fee7850c34bac2f482b36d29cd051
Angela Wirth <angiewirth92@gmail.com>
2023-03-27 17:37:27 -0400

    Create skeleton of component by declaring state variables and their corresponding functions to be updated with

--- src/Components/Game/Game.js
+++ src/Components/Game/Game.js
@@ -1,12 +1,27 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import "./Game.css"
 
-class Game extends Component {
-  constructor() {
-    super()
+ function Game()  {
+  const [deck, setDeck] = useState([])
+  const [score, setScore] = useState(0)
+  const [instructions, setInstructions] = useState(false)
+  const [leaderboard, setLeaderboard] = useState(false)
+  const [name, setName] = useState('')
+
+  const clearInputs = () => {
+    setDeck = ''
+    setScore = 0
+    setInstructions = false
+    setLeaderboard = false
+    setName = ''
   }
-}
 
+  return (
+    <div className="game-container">
+      <h1>Hi, I'm the game component!</h1>
+    </div>
+  )
 
+ }
 
 export default Game
