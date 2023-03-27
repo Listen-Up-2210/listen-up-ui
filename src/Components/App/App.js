b56ad13abc019aad0b19e2506e7451db3e060d79
Zac Walters <zacwalters4@gmail.com>
2023-03-27 14:49:41 -0600

    Add initial routes

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,9 +1,16 @@
-import logo from './logo.svg';
+
 import './App.css';
+import {Route, Routes} from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
+      {/* <Header /> */}
+      <Routes>
+        <Route path="/" element={<h1>category component</h1>}/>
+        <Route path=':category' element={<h1>difficulty component</h1>} />
+        <Route path=':category/:difficulty' element={<h1>option component</h1>} />
+      </Routes>
     </div>
   );
 }
