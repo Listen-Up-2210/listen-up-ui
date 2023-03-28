a41dc298bc9b1eddd6c0e352668dd9412da6f9c5
Angela Wirth <angiewirth92@gmail.com>
2023-03-28 10:18:01 -0400

    Remove instruction and leaderboard state because that will live in header component

--- src/Components/Game/Game.js
+++ src/Components/Game/Game.js
@@ -4,41 +4,22 @@ import "./Game.css"
  function Game()  {
   const [deck, setDeck] = useState([])
   const [score, setScore] = useState(0)
-  const [instructions, setInstructions] = useState(false)
-  const [leaderboard, setLeaderboard] = useState(false)
   const [name, setName] = useState('')
 
   const clearInputs = () => {
     setDeck = ''
     setScore = 0
-    setInstructions = false
-    setLeaderboard = false
     setName = ''
-  }
-
-  setDeck = () => {
-
   }
 
   setScore = () => {
     return score ++
   }
 
-  setInstructions = () => {
-    instructions = true
-    return instructions
-  }
-
-  setLeaderboard = () => {
-    leaderboard = true
-    return leaderboard
-  }
-
   setName = (input) => {
     name = input
     return name
   }
-
 
 
   return (
