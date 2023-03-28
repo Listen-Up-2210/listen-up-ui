1c1f323e025823a81e18f2d6e2b0debe90c05b70
Robert Aiken <robertsiraaiken@gmail.com>
2023-03-28 14:40:24 -0500

    Add header icons and header element placements

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,6 +1,8 @@
 import React, {useState} from "react";
 import "./Header.css"
 import Modal from "../Modal/Modal";
+import instructionIcon from "../../instructions-icon.png"
+import leaderboardIcon from "../../leaderboard-icon.png"
 
 export default function Header() {
     const [showModal, setShowModal] = useState(true);
@@ -18,9 +20,9 @@ export default function Header() {
   return (
     <header className='header'>
       <h1 className='title'>Listen Up!</h1>
-      <div>
-        <button className='modalBtn' onClick={e => handleShowModal(e)} value="info">Show Info</button>
-        <button className='modalBtn' onClick={e => handleShowModal(e)} value="leader">Show Leader</button>
+      <div className='modal-icons-container'>
+        <img src={instructionIcon} alt="instructions" className='modal-button' onClick={e => handleShowModal(e)} value="info"/>
+        <img src={leaderboardIcon} alt="leaderboard" className='modal-button' onClick={e => handleShowModal(e)} value="leader"/>
         <Modal show={showModal} handleClose={handleCloseModal} display={modalContent} />
       </div>
     </header>
