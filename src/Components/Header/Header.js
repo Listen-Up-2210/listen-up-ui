188fd9f2774746de8c042ea1a6e4f3d8c9c839a4
Matt Rowan <matt.rowan121@gmail.com>
2023-03-27 20:38:03 -0400

    Update buttons for modal popouts

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,59 +1,28 @@
 import React, {useState} from "react";
-import ReactDOM from 'react-dom'
 import "./Header.css"
-import Instruction from "../Instruction/Instruction";
-import Leaderboard from "../LeaderBoard/LeaderBoard";
-
-// function Modal(props) {
-//   return ReactDOM.createPortal(
-//     <div id="modal-root" className="modal">
-//       <div className="modal-content">
-//         {props.children}
-//       </div>
-//     </div>,
-//     document.getElementById('modal-root')
-//   );
-// }
+import Modal from "../Modal/Modal";
 
 export default function Header() {
+    const [showModal, setShowModal] = useState(true);
+    const [modalContent, setModalContent] = useState('info')
 
-  const[showInstruction, setShowInstruction] = useState(true)
-  const[showLeaderboard, setShowLeaderboard] = useState(false)
+    const handleShowModal = (e) => {
+        setShowModal(true);
+        setModalContent(e.target.value)
+    };
 
-  const handleOpenInstruction = () => {
-    setShowInstruction(true)
-  }
-
-  const handleCloseInstruction = () => {
-    setShowInstruction(false)
-  }
-
-  const handleOpenLeaderboard = () => {
-    setShowLeaderboard(true)
-  }
-
-  const handleCloseLeaderboard = () => {
-    setShowLeaderboard(false)
-  }
+    const handleCloseModal = () => {
+        setShowModal(false);
+    };
 
   return (
-    <h1 className='header'>
-      <p className='title'>Listen Up!</p>
+    <header className='header'>
+      <h1 className='title'>Listen Up!</h1>
       <div>
-        <button className='modal-buttons' onClick={handleOpenInstruction}>Information</button>
-        {showInstruction && 
-        <Instruction>
-          <h2>Hello</h2>  
-          <button onClick={handleCloseInstruction}>Close Modal</button>
-        </Instruction>}
-        <button className='modal-buttons' onClick={handleOpenLeaderboard}>Leaderboard</button>
-        {showLeaderboard && 
-        <Leaderboard>
-          <h2>LeaderBoard</h2>  
-          <button onClick={handleCloseLeaderboard}>Close Modal</button>
-        </Leaderboard>}
+        <button className='modalBtn' onClick={e => handleShowModal(e)} value="info">Show Info</button>
+        <button className='modalBtn' onClick={e => handleShowModal(e)} value="leader">Show Leader</button>
+        <Modal show={showModal} handleClose={handleCloseModal} display={modalContent} />
       </div>
-    </h1>
-
+    </header>
   )
 }
