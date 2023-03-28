d7402218181a6787a8c36dcdbdcd1050ec8c98bf
Zac Walters <zacwalters4@gmail.com>
2023-03-27 22:16:23 -0600

    Create initial category layout

--- src/Components/Category/Category.js
+++ src/Components/Category/Category.js
@@ -1,7 +1,17 @@
 import React from "react";
 import "./Category.css"
 
+function Category() {
 
+    return (
+        <div className="categories">
+            <button>Animals</button>
+            <button>Instruments</button>
+            <button>Machines</button>
+            <button>Miscellaneous</button>
+        </div>
+    )
+}
 
 
 export default Category
