4e8fdbb73a78adb7ca8e7a66e3b0c1bfc9f7c19b
Matt Rowan <matt.rowan121@gmail.com>
2023-03-29 12:45:06 -0400

    Remove unnecessary default state and unused 'e'

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -6,7 +6,7 @@ import leaderboardIcon from "../../leaderboard-icon.png"
 
 export default function Header() {
     const [showModal, setShowModal] = useState(true);
-    const [modalContent, setModalContent] = useState('info')
+    const [modalContent, setModalContent] = useState('')
 
     const handleShowModal = (content) => {
       setShowModal(true);
@@ -21,8 +21,8 @@ export default function Header() {
     <header className='header'>
       <h1 className='title'>Listen Up!</h1>
       <div className='modal-icons-container'>
-        <img src={instructionIcon} alt="instructions" className='modal-button' onClick={e => handleShowModal("value")}/>
-        <img src={leaderboardIcon} alt="leaderboard" className='modal-button' onClick={e => handleShowModal("leader")}/>
+        <img src={instructionIcon} alt="instructions" className='modal-button' onClick={handleShowModal("value")}/>
+        <img src={leaderboardIcon} alt="leaderboard" className='modal-button' onClick={handleShowModal("leader")}/>
         <Modal show={showModal} handleClose={handleCloseModal} display={modalContent} />
       </div>
     </header>
