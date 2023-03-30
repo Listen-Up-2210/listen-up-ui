31ef0ed3a18d399bbb9c65df5e9462519f756bad
Natalie Kinsaul <n.kinsaul@gmail.com>
2023-03-30 08:46:08 -0700

    Add basic styling to game-card-container

--- src/Components/Choices/Choices.js
+++ src/Components/Choices/Choices.js
@@ -1,7 +1,10 @@
+
 import React, { useState } from "react";
 import './Choices.css'
 
 const Choices = ({correctAnswer, wrongAnswers, advanceTurn}) => {
+
+  const [button, setButton] = useState(false)
 
   const answers = [...wrongAnswers, correctAnswer]
   const shuffledAnswers = answers.sort(() => Math.random() - .5)
@@ -9,8 +12,11 @@ const Choices = ({correctAnswer, wrongAnswers, advanceTurn}) => {
   const checkAnswer = (e) => {
     e.target.name === correctAnswer ? e.target.className = 'correct' :
     e.target.className = 'incorrect'
+    setButton(true)
     advanceTurn(e)
   }
+
+
 
   const answerButtons = shuffledAnswers?.map((answer, index) => {
     return (
@@ -19,6 +25,7 @@ const Choices = ({correctAnswer, wrongAnswers, advanceTurn}) => {
         id={index}
         key={index}
         name={answer}
+        disabled={button}
         >{answer}
       </button>
     )
