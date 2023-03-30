d272cee5dfdc13e37e1ee67d2f16b5a14239453f
Natalie Kinsaul <n.kinsaul@gmail.com>
2023-03-30 12:16:13 -0700

    Add mock data to Question component to simulate fetch request

--- src/Components/Game/Game.js
+++ src/Components/Game/Game.js
@@ -1,46 +1,67 @@
 import React, { useEffect, useState } from "react";
 import "./Game.css"
-// import Question from './Question/Question'
+import Question from '../Question/Question'
 import { useLocation } from 'react-router-dom'
 
  function Game()  {
-  const [deck, setDeck] = useState([])
+  // const [deck, setDeck] = useState([])
   let location = useLocation().pathname.split("/")
 
-  useEffect(() => {
-    console.log('LOCATION', location)
-    const categoryQuery = `
-      query {
-        sound_cards(category: location[1]) {
-          id:
-          correct answer:
-          wrong answers:
-        }
-      }
-    `
+  const deck = [
+    {
+      id: 16933,
+      audioURL: "https://cdn.freesound.org/previews/16/16933_37876-hq.mp3",
+      correctAnswer: "Donkey",
+      wrongAnswers: ["Horse", "Goat", "Cow"]
+    },
+    {
+      id: 73371,
+      audioURL: "https://cdn.freesound.org/previews/73/73371_238949-hq.mp3",
+      correctAnswer: "Bee",
+      wrongAnswers: ["Hornet", "Humming Bird", "Mosquito"]
+    },
+    {
+      id: 510917,
+      audioURL: "https://cdn.freesound.org/previews/510/510917_11157357-hq.mp3",
+      correctAnswer: "Seagull",
+      wrongAnswers: ["Pelican", "Eagle", "Hawk"],
+    }
+  ]  
 
-    fetch("URL", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify({
-        query: categoryQuery
-      })
-    })
-    .then(res => res.json())
-    .then(data => {
-      console.log(data)
-      setDeck(data)
-    })
-    .catch(err => console.log(err))
-  }, []) 
+  // useEffect(() => {
+  //   console.log('LOCATION', location)
+  //   const categoryQuery = `
+  //     query {
+  //       sound_cards(category: location[1]) {
+  //         id:
+  //         correct answer:
+  //         wrong answers:
+  //       }
+  //     }
+  //   `
+
+  //   fetch("URL", {
+  //     method: "POST",
+  //     headers: {
+  //       "Content-Type": "application/json"
+  //     },
+  //     body: JSON.stringify({
+  //       query: categoryQuery
+  //     })
+  //   })
+  //   .then(res => res.json())
+  //   .then(data => {
+  //     console.log(data)
+  //     setDeck(data)
+  //   })
+  //   .catch(err => console.log(err))
+  // }, []) 
   
 
   return (
-    <div className="game-container">
-      {/* <Question deck={deck}/> */}
-    </div>
+    // <div className="game-container">
+      <Question deck={deck}/>
+    // </div>
   )
 
  }
--- src/Components/Question/Question.js
+++ src/Components/Question/Question.js
@@ -3,28 +3,28 @@ import "./Question.css"
 import Audio from '../Audio/Audio'
 import Choices from "../Choices/Choices";
 
-const Question = () => {
+const Question = ({deck}) => {
 
-  const deck = [
-    {
-      id: 16933,
-      audioURL: "https://cdn.freesound.org/previews/16/16933_37876-hq.mp3",
-      correctAnswer: "Donkey",
-      wrongAnswers: ["Horse", "Goat", "Cow"]
-    },
-    {
-      id: 73371,
-      audioURL: "https://cdn.freesound.org/previews/73/73371_238949-hq.mp3",
-      correctAnswer: "Bee",
-      wrongAnswers: ["Hornet", "Humming Bird", "Mosquito"]
-    },
-    {
-      id: 510917,
-      audioURL: "https://cdn.freesound.org/previews/510/510917_11157357-hq.mp3",
-      correctAnswer: "Seagull",
-      wrongAnswers: ["Pelican", "Eagle", "Hawk"],
-    }
-  ]  
+  // const deck = [
+  //   {
+  //     id: 16933,
+  //     audioURL: "https://cdn.freesound.org/previews/16/16933_37876-hq.mp3",
+  //     correctAnswer: "Donkey",
+  //     wrongAnswers: ["Horse", "Goat", "Cow"]
+  //   },
+  //   {
+  //     id: 73371,
+  //     audioURL: "https://cdn.freesound.org/previews/73/73371_238949-hq.mp3",
+  //     correctAnswer: "Bee",
+  //     wrongAnswers: ["Hornet", "Humming Bird", "Mosquito"]
+  //   },
+  //   {
+  //     id: 510917,
+  //     audioURL: "https://cdn.freesound.org/previews/510/510917_11157357-hq.mp3",
+  //     correctAnswer: "Seagull",
+  //     wrongAnswers: ["Pelican", "Eagle", "Hawk"],
+  //   }
+  // ]  
 
   const [turn, setTurn] = useState(0)
 
