1767c6ad62036492d523c42709ee131db5ebd4ad
Matt Rowan <matt.rowan121@gmail.com>
2023-03-30 16:04:56 -0400

    Add instruction content to modal

--- src/Components/Modal/Modal.js
+++ src/Components/Modal/Modal.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {IoCloseSharp} from 'react-icons/io5'
 import './Modal.css'
 
 const Modal = ({ show, handleClose, display }) => {
@@ -8,15 +9,24 @@ const Modal = ({ show, handleClose, display }) => {
     <div className={showHideClassName}>
         {display === 'leader' ?
             <section className="modal-main">
+                <IoCloseSharp className='close-Btn' onClick={handleClose} />
                 <h2>Leaderboard Content</h2>
                 <p>This is the content of the modal.</p>
-                <button onClick={handleClose}>Close</button>
             </section>
                 :
             <section className="modal-main">
-                <h2>Instructions</h2>
-                <p>To play the game, click the play button to hear a sound and then select one of the four choices provided to submit your guess. There are eight questions a game. Good luck!</p>
-                <button onClick={handleClose}>Close</button>
+                <IoCloseSharp className='close-Btn' onClick={handleClose} />
+                <h2>How To Play</h2>
+                <ol>
+                    <li>Choose a category of sounds that you want to play with.</li>
+                    <li>Choose a difficulty level that you are comfortable with.</li>
+                        <ul>
+                            <li>Easy: 3 guesses per sound</li>
+                            <li>Medium: 2 guesses per sound</li>
+                            <li>Hard: 1 guess per sound</li>
+                        </ul>
+                    <li>After you have guessed all eight sounds, your score will be displayed.</li>
+                </ol>
             </section>
         }
     </div>
