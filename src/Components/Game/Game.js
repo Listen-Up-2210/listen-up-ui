0339fc78b3be0493a1422bcf13d0c3a95a77b593
Robert Aiken <robertsiraaiken@gmail.com>
2023-03-30 15:18:49 -0500

    Add connection to database

--- src/Components/Game/Game.js
+++ src/Components/Game/Game.js
@@ -4,59 +4,59 @@ import Question from '../Question/Question'
 import { useLocation } from 'react-router-dom'
 
  function Game()  {
-  // const [deck, setDeck] = useState([])
+  const [deck, setDeck] = useState([])
   let location = useLocation().pathname.split("/")
 
-  const deck = [
-    {
-      id: 16933,
-      audioURL: "https://cdn.freesound.org/previews/16/16933_37876-hq.mp3",
-      correctAnswer: "Donkey",
-      wrongAnswers: ["Horse", "Goat", "Cow"]
-    },
-    {
-      id: 73371,
-      audioURL: "https://cdn.freesound.org/previews/73/73371_238949-hq.mp3",
-      correctAnswer: "Bee",
-      wrongAnswers: ["Hornet", "Humming Bird", "Mosquito"]
-    },
-    {
-      id: 510917,
-      audioURL: "https://cdn.freesound.org/previews/510/510917_11157357-hq.mp3",
-      correctAnswer: "Seagull",
-      wrongAnswers: ["Pelican", "Eagle", "Hawk"],
-    }
-  ]  
+  // const deck = [
+  //   // {
+  //   //   id: 16933,
+  //   //   audioURL: "https://cdn.freesound.org/previews/16/16933_37876-hq.mp3",
+  //   //   correctAnswer: "Donkey",
+  //   //   wrongAnswers: ["Horse", "Goat", "Cow"]
+  //   // },
+  //   // {
+  //   //   id: 73371,
+  //   //   audioURL: "https://cdn.freesound.org/previews/73/73371_238949-hq.mp3",
+  //   //   correctAnswer: "Bee",
+  //   //   wrongAnswers: ["Hornet", "Humming Bird", "Mosquito"]
+  //   // },
+  //   // {
+  //   //   id: 510917,
+  //   //   audioURL: "https://cdn.freesound.org/previews/510/510917_11157357-hq.mp3",
+  //   //   correctAnswer: "Seagull",
+  //   //   wrongAnswers: ["Pelican", "Eagle", "Hawk"],
+  //   // }
+  // ]  
 
-  // useEffect(() => {
-  //   console.log('LOCATION', location)
-  //   const categoryQuery = `
-  //     query {
-  //       soundCardsByCategory(category: location[1]) {
-  //         id
-  //         correctAnswer
-  //         wrongAnswers
-  //         link
-  //       }
-  //     }
-  //   `
+  useEffect(() => {
+    console.log("Location" , location[1])
+    const categoryQuery = `
+      query {
+        soundCardsByCategory(category: "${location[1]}") {
+          id
+          correctAnswer
+          wrongAnswers
+          link
+        }
+      }
+    `
 
-  //   fetch("https://listen-up-be.herokuapp.com/graphql", {
-  //     method: "POST",
-  //     headers: {
-  //       "Content-Type": "application/json"
-  //     },
-  //     body: JSON.stringify({
-  //       query: categoryQuery
-  //     })
-  //   })
-  //   .then(res => res.json())
-  //   .then(data => {
-  //     console.log(data)
-  //     setDeck(data)
-  //   })
-  //   .catch(err => console.log(err))
-  // }, []) 
+    fetch("https://listen-up-be.herokuapp.com/graphql", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        query: categoryQuery
+      })
+    })
+    .then(res => res.json())
+    .then(data => {
+      console.log(data)
+      setDeck(data.data.soundCardsByCategory)
+    })
+    .catch(err => console.log(err))
+  }, []) 
   
 
   return (
