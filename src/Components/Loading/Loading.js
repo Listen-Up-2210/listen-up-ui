aeb002c87deeae08b64992dcd79697d8320a6a2f
Robert Aiken <robertsiraaiken@gmail.com>
2023-03-31 18:05:45 -0500

    Add higher quality loading animation

--- src/Components/Loading/Loading.js
+++ src/Components/Loading/Loading.js
@@ -6,8 +6,8 @@ export default function Loading(){
 
   return (
     <section className="loading-container">
-      <img src={loadingIcon} className="loading-icon"/>
-      <p> Game is Loading, Please Wait </p>
+      <div class="loader"></div>
+      <p className="loading-text"> Game is Loading, Please Wait </p>
     </section>
   )
 }
