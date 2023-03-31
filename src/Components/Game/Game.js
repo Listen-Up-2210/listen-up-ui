09cf6420d520ecf48455dd641194603db53a60fe
Zac Walters <zacwalters4@gmail.com>
2023-03-31 11:17:04 -0600

    Add loading and error display to game.js

--- src/Components/Game/Game.js
+++ src/Components/Game/Game.js
@@ -1,17 +1,18 @@
-import React, { useEffect, useState } from "react";
+import React, { Fragment, useEffect, useState } from "react";
 import "./Game.css"
 import Question from '../Question/Question'
 import { useLocation, useNavigate } from 'react-router-dom'
+import ErrorDisplay from "../ErrorDisplay/ErrorDisplay"
 
  function Game()  {
   const [deck, setDeck] = useState([])
+  const [error, setError] = useState('')
   const categories = ['animals', 'instruments', 'machines', 'misc']
   const difficulties = ['easy', 'medium', 'hard']
   const navigate = useNavigate()
   let location = useLocation().pathname.split("/")
 
   useEffect(() => {
-
     if(!categories.includes(location[1]) || !difficulties.includes(location[2])) {
       navigate('/error/404')
   }
@@ -40,11 +41,15 @@ import { useLocation, useNavigate } from 'react-router-dom'
       console.log(data)
       setDeck(data.data.soundCardsByCategory)
     })
-    .catch(err => console.log(err))
+    .catch(err => setError(err))
   }, []) 
   
   return (
-    <Question deck={deck}/>
+    <Fragment>
+      {(deck.length === 0 && !error) && <p>Loading...</p>}
+      {error && <h1>Something went wrong, please try again.</h1>}
+      <Question deck={deck}/>
+    </Fragment>
   )
  }
 
