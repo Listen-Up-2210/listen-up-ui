e49ce20239e2186d05546ff13ce6edf6432411f0
Robert Aiken <robertsiraaiken@gmail.com>
2023-03-31 14:54:51 -0500

    Add leaderboard post request

--- src/Components/Endgame/Endgame.js
+++ src/Components/Endgame/Endgame.js
@@ -2,7 +2,21 @@ import React from "react";
 import './Endgame.css'
 import { Navlink } from 'react-router-dom'
 
-function Endgame() {
+
+function Endgame(props) {
+
+  const updateLeaderboard = () => {
+    fetch("URL", {
+      method: `POST`,
+      headers: {'Content-Type':'application/json'},
+      body: JSON.stringify({
+
+      })
+    })
+    .then(res => res.json())
+    .then(data => console.log(data))
+    .catch(error => console.error(error))
+  }
 
   return (
     <div>
