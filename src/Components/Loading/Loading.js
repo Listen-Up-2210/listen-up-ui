cc6b92707955f23e0e940238584343efc901866b
Robert Aiken <robertsiraaiken@gmail.com>
2023-03-31 14:40:32 -0500

    Add initial styling

--- src/Components/Loading/Loading.js
+++ src/Components/Loading/Loading.js
@@ -1,11 +1,13 @@
 import React from "react";
 import loadingIcon from "../../icons8-loading-circle.gif"
+import './Loading.css'
 
 export default function Loading(){
 
   return (
     <section className="loading-container">
-      <img src={loadingIcon}/>
+      <img src={loadingIcon} className="loading-icon"/>
+      <p> Game is Loading, Please Wait </p>
     </section>
   )
 }
