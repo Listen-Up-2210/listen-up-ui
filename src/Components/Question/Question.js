18576632e36c017101cf9402e208cd84f2ecfb0a
Matt Rowan <matt.rowan121@gmail.com>
2023-04-03 15:15:48 -0400

    Update fetch request and refactor component return

--- src/Components/Question/Question.js
+++ src/Components/Question/Question.js
@@ -6,7 +6,7 @@ import Choices from "../Choices/Choices";
 const Question = ({ deckID }) => {
 
   const [turn, setTurn] = useState(0)
-  const [card,setCard] = useState({})
+  const [card,setCard] = useState([])
 
   const advanceTurn = (e) => {
     e.preventDefault()
@@ -14,7 +14,6 @@ const Question = ({ deckID }) => {
   }
 
   useEffect(() => {
-    console.log(deckID)
     const cardQuery = `
     query {
         soundCard(deckId: ${deckID}) {
@@ -37,31 +36,30 @@ const Question = ({ deckID }) => {
     })
     .then(res => res.json())
     .then(data => {
-      console.log(data)
-      setCard(data.data.soundCard)
+      setCard([data.data.soundCard])
     })
     .catch(err => console.log(err))
-  }, [])
+  }, [deckID])
 
-  const gameCards = () => {
-    const answers = [...card.wrongAnswers, card.correctAnswer]
-    const shuffledAnswers = answers.sort(() => Math.random() - .5)
-    return (
-      <div className='card' key={card.id}>
-        <Audio audioURL={card.link} />
-        <Choices
-        advanceTurn={advanceTurn} 
-        correctAnswer={card.correctAnswer}
-        shuffledAnswers={shuffledAnswers}
-        />
-      </div>
-    )
-  }
+  const gameCard = card.map((card, index) => {
+      const answers = [...card.wrongAnswers, card.correctAnswer]
+      const shuffledAnswers = answers.sort(() => Math.random() - .5)
+      return (
+        <div className='card' key={index}>
+          <Audio audioURL={card.link} />
+          <Choices
+            advanceTurn={advanceTurn} 
+            correctAnswer={card.correctAnswer}
+            shuffledAnswers={shuffledAnswers}
+          />
+        </div>
+      )
+    })
   
 
   return (
     <div className="game-card-container">
-      {gameCards}
+      {gameCard ? gameCard : <h2>Loading</h2>}
     </div>
   )
 }
