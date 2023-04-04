fbf2acb3dc891d8c411960f098aae852e70cbeb8
Angela Wirth <angiewirth92@gmail.com>
2023-04-04 17:25:33 -0400

    Update navlink to update state in app, WIP

--- src/Components/Endgame/Endgame.js
+++ src/Components/Endgame/Endgame.js
@@ -1,9 +1,61 @@
-import React from "react";
+import React, { useState, useEffect} from "react";
+import './Endgame.css'
+import { NavLink } from 'react-router-dom'
+import Modal from "../Modal/Modal";
 
-const EndGame = ({correctAnswers}) => {
-  return (
-    <h2>You got {correctAnswers} right</h2>
-  )
+
+function Endgame({correctAnswers, category, difficulty}) {
+ const [name, setName] = useState("")
+
+ const submitScore = () => {
+  const score = correctAnswers *100
+   const scorePost = `
+     mutation createLeaderBoard {
+       createLeaderboard(input: {
+         name: "${name}"
+         score: ${score}
+         category: "${category}"
+         difficulty: "${difficulty}"
+       }) {
+         leaderboard {
+           name
+           score
+           difficulty
+           category
+         }
+         errors
+       }
+     }
+     `
+
+
+     fetch("https://listen-up-be.herokuapp.com/graphql", {
+       method: "POST",
+       headers: {
+         "Content-Type": "application/json"
+       },
+       body: JSON.stringify({
+         query: scorePost
+       })
+     })
+     .then(res => res.json())
+     .then(data => {
+       console.log('POSTED', data)
+     })
+     .then(setName(""))
+     .catch(err => console.log(err))
+   }
+
+ return (
+   <div>
+     <h2>You got props out of 8 questions correct!</h2>
+     <form className="form-container">
+       <input type="text" name="username" placeholder="Enter name here" value={name} onChange={e => setName(e.target.value)} autoComplete="off" required />
+       <NavLink to={{pathname: "/", state: {gameEnded: true}}}><button onClick={submitScore}>Submit</button></NavLink>
+       {/* <Modal show={true} handleClose={handleCloseModal} display={"leader"} /> */}
+     </form>
+   </div>
+ )
 }
 
-export default EndGame
+export default Endgame
