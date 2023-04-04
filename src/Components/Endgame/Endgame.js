83f99dc2e38d97bb48e89c47d574c303c26cae32
Angela Wirth <angiewirth92@gmail.com>
2023-04-04 17:54:52 -0400

    Import react context

--- src/Components/Endgame/Endgame.js
+++ src/Components/Endgame/Endgame.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect} from "react";
+import React, { useState, useEffect, useContext} from "react";
 import './Endgame.css'
 import { NavLink } from 'react-router-dom'
 import Modal from "../Modal/Modal";
@@ -6,6 +6,7 @@ import Modal from "../Modal/Modal";
 
 function Endgame({correctAnswers, category, difficulty}) {
  const [name, setName] = useState("")
+//  const gameState = useContext(gameContext)
  const score = correctAnswers *100
 
  const submitScore = () => {
@@ -28,7 +29,6 @@ function Endgame({correctAnswers, category, difficulty}) {
      }
      `
 
-
      fetch("https://listen-up-be.herokuapp.com/graphql", {
        method: "POST",
        headers: {
@@ -44,15 +44,16 @@ function Endgame({correctAnswers, category, difficulty}) {
      })
      .then(setName(""))
      .catch(err => console.log(err))
+     
    }
 
  return (
    <div>
-     <h2>You got ${correctAnswers} out of 8 questions correct!</h2>
-     <h3>You got a score of ${score}</h3>
+     <h2>You got {correctAnswers} out of 8 questions correct!</h2>
+     <h3>You got a score of {score}</h3>
      <form className="form-container">
        <input type="text" name="username" placeholder="Enter name here" value={name} onChange={e => setName(e.target.value)} autoComplete="off" required />
-       <NavLink to={{pathname: "/", state: {gameEnded: true}}}><button onClick={submitScore}>Submit</button></NavLink>
+       <NavLink to={{pathname: "/", state: {gameEnded: true}}}><button onClick={submitScore} game={true}>Submit</button></NavLink>
        {/* <Modal show={true} handleClose={handleCloseModal} display={"leader"} /> */}
      </form>
    </div>
