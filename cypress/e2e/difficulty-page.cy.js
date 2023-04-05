b2c28f8ed7e584dccc5c4e493846e55c05faccdb
Matt Rowan <matt.rowan121@gmail.com>
2023-04-05 18:08:26 -0400

    Add intercept to test game load

--- cypress/e2e/difficulty-page.cy.js
+++ cypress/e2e/difficulty-page.cy.js
@@ -1,4 +1,4 @@
-import { aliasQuery, aliasMutation } from '../utils/graphql-test-utils'
+import { aliasQuery } from '../utils/graphql-test-utils'
 
 describe('template spec', () => {
   beforeEach(() => {
@@ -53,13 +53,23 @@ describe('template spec', () => {
 
   it('should be taken to a new page after selecting a difficulty', () => {
     cy.get('.easy')
-    .click()
-    cy.intercept("POST", 'https://listen-up-be.herokuapp.com/graphql', {fixture: "soundCard"})  
+    .click() 
+    cy.intercept('POST', 'https://listen-up-be.herokuapp.com/graphql', (req) => {
+      aliasQuery(req, '')
+      req.reply(
+        {fixture:'soundCard.json'}
+      )
+    })
     cy.url()
       .should('eq','http://localhost:3000/animals/easy')
       .should('contain', 'animals')
       .should('contain', 'easy')
-    cy.get('[data-cy="button"]').should('exist')
-    cy.get('[data-cy="counter"]').should('contain', 'Question: 1 / 8')
+    cy.get('.turn-count').should('contain', 'Question: 1 / 8')
+    cy.get('.choices-container').children()
+    .should('have.length', 4)
+    .should('contain', 'Cicada')
+    .should('contain', 'monkey')
+    .should('contain', 'cricket')
+    .should('contain', 'monkey')
   })
 })
