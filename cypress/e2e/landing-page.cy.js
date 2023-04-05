8ea7e78d3ea6456c103cfd3780cf9afffcc3aaf8
Matt Rowan <matt.rowan121@gmail.com>
2023-04-05 17:30:27 -0400

    Recreate landing page test

---
+++ cypress/e2e/landing-page.cy.js
@@ -0,0 +1,121 @@
+describe('template spec', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:3000/')
+  })
+  
+  it('should have an instruction modal visible on-load', () => {
+    cy.get('.modal-main')
+      .should('be.visible')
+  })
+
+  it('should display "How To Play" in the modal ', () => {
+    cy.get('.modal-main > h2')
+      .should('contain', 'How To Play')
+  })
+
+  it('should have an ordered list with 3 instructions', () => {
+    cy.get('.modal-main > ol')
+    .children()
+    .find('li')
+    .should('have.length', 3)
+    .get('.step-1')
+    .should('contain', 'Choose a category of sounds that you want to play with.')
+    .get('.step-2')
+    .should('contain', 'Choose a difficulty level that you are comfortable with.')
+    .get('.step-3')
+    .should('contain', 'After you have guessed all eight sounds, your score will be displayed.')
+  })
+
+  it('should have an unorderd list that explains difficulties', () => {
+    cy.get('.modal-main > ol')
+    .find('ul')
+    .children()
+    .should('have.length', 3)
+    .get('.ex-easy')
+    .should('contain', 'Easy: 3 guesses per sound')
+    .get('.ex-medium')
+    .should('contain', 'Medium: 2 guesses per sound')
+    .get('.ex-hard')
+    .should('contain', 'Hard: 1 guess per sound')
+  })
+
+  it('should have a button to close the modal', () => {
+    cy.get('.close-Btn')
+      .should('be.visible')
+  })
+
+  it('should close the modal when the button is clicked ', () => {
+    cy.get('.close-Btn')
+      .click()
+    cy.get('.modal-main')
+      .should('not.be.visible')
+  })
+
+  it('should display a "Listen Up" header',() => {
+    cy.get('.close-Btn')
+      .click()
+    cy.get('.header > h1')
+      .should('contain', 'Listen Up!')
+  })
+
+  it('should have an info and stats button in the header',() => {
+    cy.get('.close-Btn')
+      .click()
+    cy.get('.modal-icons-container')
+      .get('img[alt="instructions"]')
+      .should('be.visible')
+      .get('img[alt="leaderboard"]')
+      .should('be.visible')
+  })
+
+  it('should display the instructions modal when the button is clicked', () => {
+    cy.get('.close-Btn')
+      .click()
+    cy.get('.modal-icons-container')
+      .get('img[alt="instructions"]')
+      .click()
+    cy.get('.modal-main > h2')
+      .should('contain', 'How To Play')
+  })
+
+  it('should display the leaderboard modal when the button is clicked', () => {
+    cy.get('.close-Btn')
+      .click()
+    cy.get('.modal-icons-container')
+      .get('img[alt="leaderboard"]')
+      .click()
+    cy.get('.modal-main > h2')
+      .should('contain', 'Leaderboard Content')
+  })
+
+  it('should have four categories displayed',() => {
+    cy.get('.close-Btn')
+      .click()
+      .get('.categories')
+      .children()
+      .should('have.length', 4)
+  })
+
+  it('should have the correct text in each category button', () => {
+    cy.get('.close-Btn')
+      .click()
+      .get('a[href="/animals"]')
+        .should('contain', 'Animals')
+      .get('a[href="/instruments"]')
+        .should('contain', 'Instruments')
+      .get('a[href="/machines"]')
+        .should('contain', 'Machines')
+      .get('a[href="/misc"]')
+        .should('contain', 'Miscellaneous')
+  })
+
+  it('should take a user to the difficulty page when a category is selected',() => {
+    cy.get('.close-Btn')
+      .click()
+      .get('a[href="/animals"]')
+      .click()
+    cy.url()
+      .should('eq','http://localhost:3000/animals')
+      .should('contain', 'animals')
+  })
+})
