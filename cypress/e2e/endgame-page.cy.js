2a95707fe305bdb626b710576a2fe83ee44a8baa
Matt Rowan <matt.rowan121@gmail.com>
2023-04-05 19:19:53 -0400

    Add initial tests for end game form

---
+++ cypress/e2e/endgame-page.cy.js
@@ -0,0 +1,49 @@
+import { aliasQuery, aliasMutation } from '../utils/graphql-test-utils'
+import gameplayTest from '../utils/gameplay-test'
+
+describe('template spec', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:3000/animals')
+    cy.get('.easy')
+    .click()
+    cy.intercept('POST', 'https://listen-up-be.herokuapp.com/graphql', (req) => {
+      aliasQuery(req, '')
+      req.reply(
+        {fixture:'soundCard.json'}
+      )
+    })
+    gameplayTest()
+  })
+
+  it.skip("should display how many quesitons a user got correct and a score", () => {
+    cy.get('h2')
+      .should('contain', 'You got 8 out of 8 questions correct!')
+  })
+
+  it.skip("should display a user's score", () => {
+    cy.get('h3')
+    .should('contain', 'You got a score of 800')
+  })
+
+  it.skip("should have a form to submit a users name", () => {
+    cy.get('form')
+      .should('be.visible')
+    cy.get('input')
+    .type('Hugh Jass')
+  })
+
+  it.only("should be able to submit the score", () => {
+    cy.get('input')
+      .type('Hugh Jass')
+    cy.get('button')
+      .click()
+    cy.url()
+      .should('eq','http://localhost:3000/')
+    cy.intercept('POST', 'https://listen-up-be.herokuapp.com/graphql', (req) => {
+        aliasMutation(req, 'createLeaderBoard')
+        req.reply(
+          {fixture:'newScoreEntry.json'}
+        )
+      })
+  })
+})
